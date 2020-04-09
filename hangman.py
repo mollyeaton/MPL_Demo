63a60ffa04803c3b1e1a2226770a09b0ace30b71
mollyeaton <molly.a.eaton@gmail.com>
2020-04-09 10:52:28 -0400

    had to change how files were opened and closed update python code

--- hangman.py
+++ hangman.py
@@ -1,22 +1,40 @@
-import numpy as np 
-import pandas as pd
-import os
 
-word = pd.read_csv(os.getcwd() + '/word.txt')
-guesses = pd.read_csv(os.getcwd() + '/guesses.txt')
+hiddenWordFile = open("hiddenWord.txt", "w")
 
-#if they have guessed the word in full then 
-if (guesses[-1] == word[0]):
-    print("You guessed the word - " + word[0] + " - correctly! Congratulations!")
-    exit()
+wordFile = open("Word.txt", "r")
+word = wordFile.read()
+word = list(word)
+
+    
+guessesFile = open("guesses.txt", "r")
+guesses = guessesFile.read()
 
 
-for i in word[0]:
+hiddenWord = '' #initialize as an empty string
+
+#replace all letters in the word with an asterick
+for i in word:
+    hiddenWord += '*'
+
+#convert into a list so it can be manipulated
+hiddenWord = list(hiddenWord)
+
+#loop through all of the guesses and replace the astericks with letters if they are correct guesses
+for i in range(len(word)):
     for j in guesses:
         
-        if (j == i):
-            print(j, end = "")
-        else:
-            print("*", end = "")
+        if (j.lower() == word[i]):
+            #if the guess is the same as a letter show the letter
+            hiddenWord[i] = j.lower()
 
+#join the list back together into a string
+hiddenWord = "".join(hiddenWord)
+#print the new hidden word
+print(hiddenWord)
+#add the hidden word to the file so the main program can check if it is done or not
+hiddenWordFile.write(hiddenWord)
 
+#close all the files
+hiddenWordFile.close()
+wordFile.close()
+guessesFile.close()
--- main.cpp
+++ main.cpp
@@ -5,86 +5,118 @@
 #include <ctime>
 #include <iomanip>
 
+
 using namespace std;
 
-void pickWord(string inFile, string outFile);
+string pickWord(string inFile, string outFile);
 bool validateGuess(string guess);
+void clearFile(string filename);
+string makeLower(string word);
 
 int main(){
     //seed the random number generator
     srand((unsigned) time(0));
 
     //declare variables
-    const int MAX_GUESSES = 8;
-    int guessCounter = 0; //keeps running count of how manny guesses have been made
+    const int MAX_GUESSES = 15;
     string guess;
+    int guessCounter = 0;
+    bool guessed = false;
+    string hiddenWord;
+    string word;
 
-    //pick the word that will be used and store into the word file
-    pickWord("1-521.txt", "word.txt");
-
-    fstream fOut;
-    //open the file and clear the current contents
-    fOut.open(string("../") + "guesses.txt", ios::out | ios::trunc);
-
-
+    //clear files before running game
+    clearFile("word.txt");
+    clearFile("guesses.txt");
 
     //display intro message
     cout << "Welcome to HANGMAN!" << endl;
     cout << "The goal of the game is to guess the hidden word." << endl;
     cout << "You will have " << MAX_GUESSES << " guesses, good luck!" << endl;
 
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" <<endl;
+
+    //pick the word that will be used and store into the word file
+    word = pickWord("1-521.txt", "word.txt");
+
     //continue the loop for the game while the player still has guesses
-    while (guessCounter < 8) {
+    while (guessCounter < MAX_GUESSES && !guessed) {
         cout << "Make a guess: ";
-        getline(cin, guess);
-        //make the guess al lowercase
-        for (int i = 0; i < guess.length(); ++i) {
-            tolower(guess[i]);
+        cin >> guess;
+        while (cin.fail()){
+            cin.clear();
+            cin.ignore(256, '\n');
+            cout << "That is not a valid response. Try again: " ;
+            cin >> guess;
         }
+
+        guess = makeLower(guess); //make lowercase
+
         //continue prompting for a guess until it meets the requirements
         while (!validateGuess(guess)){
             cout << "Enter your new guess: " ;
-            getline(cin, guess);
+            cin.clear();
+            cin.ignore(256, '\n');
+            cin >> guess;
 
-            for (int i = 0; i < guess.length(); ++i) {
-                tolower(guess[i]);
+            while (cin.fail()) {
+                cin.clear();
+                cin.ignore(256, '\n');
+                cout << "That is not a valid response. Try again: " ;
+                cin >> guess;
             }
-            validateGuess(guess);
+            guess = makeLower(guess);
         }
-        system("python3 hangman.py");
+
+        string command = "python3 hangman.py";
+        system(command.c_str());
         ++ guessCounter;
 
+        ifstream fIn("hiddenWord.txt");
+        fIn >> hiddenWord;
+        fIn.close();
+
+        if (hiddenWord.find('*') == string::npos ){
+            cout << "You guessed the word! Congratulations!" << endl;
+            guessed = true;
+            return 0;
+        }
+
+
+        cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" <<endl;
     }
+    cout << "Sorry, you ran out of guesses. The word was " << word << endl;
     return 0;
 }
 
-void pickWord(string inFile, string outFile) {
-    //seed the random number generator
-    srand((unsigned) time(0));
+string pickWord(string inFile, string outFile) {
 
     //create vector to hold all of the words pulled in
     vector <string> wordVec;
     string readWord; //create variable to hold each word read in
 
     //create the input stream for the file
-    ifstream fIn;
-    //find the text file and open it
-    fIn.open(string("../") + inFile);
+    ifstream fIn(inFile);
+
     //while the file still has information and is good
-    while (fIn && fIn.peek() != EOF)
-    {
+    while (fIn){
         //read in each line and store in the temp variable
-        getline(fIn, readWord);
-        wordVec.push_back(readWord); //add the word to the vector
+        fIn >> readWord;
+        if(fIn){
+            wordVec.emplace_back(readWord); //add the word to the vector
+        }
+
     }
+    fIn.close();
+
     //use the random number generator to pick a number between 0-521
     int wordChoice = (rand() % 521);
 
-    fIn.close(); //close the file
+    //fIn.close(); //close the file
 
-    fstream fOut;
+    ofstream fOut;
     //open the file and clear the current contents
-    fOut.open(string("../") + outFile, ios::out | ios::trunc);
+    fOut.open(outFile, ios::app);
 
     if (fOut) {
         fOut << wordVec[wordChoice];
@@ -92,42 +124,68 @@ void pickWord(string inFile, string outFile) {
     }
 
     fOut.close(); //close the file
+    cout << "The word you are trying to guess is a " << wordVec[wordChoice].length() << " letter word..." << endl;
 
+    return wordVec[wordChoice];
 }
 
 bool validateGuess(string guess){
-    bool passed = true;
-
+    string filename = "guesses.txt";
     string letter;
     vector<string> guessVec;
 
-    //create the input stream for the file
-    ifstream fIn;
-    //find the text file and open it
-    fIn.open(string("../") + "guesses.txt");
-    //while the file still has information and is good
-    while (fIn && fIn.peek() != EOF){
-        //read in each line and store in the temp variable
-        getline(fIn, letter);
-        guessVec.push_back(letter); //add the word to the vector
+    //if have gotten here then the guess has passed and can be put into the file
+    for (int i = 0; i < guess.length(); ++i){
+        guess[i] = tolower(guess[i]);
     }
-    fIn.close();
 
-    //loop through the vector to see if the letter has been guessed
-    for (int i = 0; i < guessVec.size(); ++i){
-        if (guess ==  guessVec[i]){
-            cout << "You have already guessed that letter/word" << endl;
-            passed = false;
+    //create the input/output stream for the file
+    ifstream inFile (filename);
+
+    while (inFile){
+        //read in each line and store in the temp variable
+        inFile >> letter;
+        if (inFile){
+            guessVec.emplace_back(letter); //add the word to the vector
+        }
+
+    }
+
+    //only check if letter or word is duplicate if the vector is not empty
+    if (! guessVec.empty()) {
+        //loop through the vector to see if the letter has been guessed
+        for (int i = 0; i < guessVec.size(); ++i) {
+            if (guess == guessVec[i]) {
+                cout << "You have already guessed that letter/word" << endl;
+                return false;
+            }
         }
     }
 
     for (int i = 0; i < guess.length(); ++i) {
         if (isdigit(guess[i]) || !isalnum(guess[i])){
             cout << "That is an invalid guess. No digits or special characters allowed" << endl;
-            passed = false;
+            return false;
         }
     }
 
-    return passed;
+    ofstream outFile(filename, ios::app);
+    outFile << guess << endl;
 
+    outFile.close();
+    return true;
+
+}
+
+void clearFile(string filename){
+    ofstream ofs;
+    ofs.open(filename, ios::out|ios::trunc);
+    ofs.close();
+}
+
+string makeLower(string word){
+    for (int i = 0; i < word.length(); ++i){
+        word[i] = tolower(word[i]);
+    }
+    return word;
 }
